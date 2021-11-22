6278885f132386dfc167123fe450b895290b1e63
vatue <37702734+vatue@users.noreply.github.com>
2021-11-22 16:42:10 +0200

    pwCheck migrated to backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,8 +9,6 @@ function Login(props) {
  
   // test to check the value
   const handleLogin = () => {
-    console.log(username, " handleLogin username");
-    console.log(password, " handleLogin password");
     login(username.value, password.value);
   }
 
@@ -19,13 +17,16 @@ function Login(props) {
     var un = username;
     var pw = password;
 
-    console.log(un, pw, " handleLogin -> login event");
+    axios.get('/login', {
+      params: {
+        name: un,
+        password: pw
+      }
+    })
 
-    // get
-    axios.get('/users')
     .then(function (response) {
       // handle success
-      pwCheck(response, un, pw);
+      console.log(response.data);
     })
 
     .catch(function (error) {
@@ -36,40 +37,6 @@ function Login(props) {
     .then(function (response) {
       // always executed
     });
-  }
-
-  function pwCheck(response, un, pw) {
-
-    var localUser = un;
-    var localPW = pw;
-    var res = response; // easier to read
-  
-    console.log("axios GET success -> pwCheck function");
-  
-    console.log(res.data); // debug
-  
-    // res is returned from db as an object array
-    // res contains a cfg file, data(db entries), headers & requests
-    // res.data = array of objects/entries returned from db
-    // res.data[0] = first returned entry
-    // in this case [0] is the first user
-  
-    for (var i = 0; i < res.data.length; i++) {
-      // while i < returned users from db -> loop
-  
-      // if the array index i is currently in, contains the same username & password
-      // as our local variables, login is successful. there's an ugly debug of what has happened
-  
-      if (res.data[i].Name === localUser && res.data[i].Password === localPW ) {
-        console.log("local user/pw ", localUser, ";", localPW, " matches db user/pw ", res.data[i].Name, ";",
-        res.data[i].Password, " on index response[", i, "]; and database idUser index", res.data[i].idUser);
-  
-        if (res.data[i].AdminAccount === 1)
-          console.log("admin account!");
-  
-        console.log("login successful");
-      }
-    }
   }
  
   return (
