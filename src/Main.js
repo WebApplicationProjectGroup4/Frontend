47add1471169504cb8ad06cfbb5ee8afa60d76d8
valtteeri <hietalavaltteri@gmail.com>
2021-11-25 16:23:22 +0200

    English and removed hardcoded json

--- src/Main.js
+++ src/Main.js
@@ -115,7 +115,7 @@ constructor(props) {
                <li> <input class="searchBar" type="text" placeholder="Search..."  onChange={ this.onChange } /> </li>
                <li> Help </li>
                <li></li>
-               <Link to="/login" ><button class="loginButton" > Kirjaudu </button></Link>
+               <Link to="/login" ><button class="loginButton" > Login </button></Link>
             </ul>
           </nav>
             <Routes>
