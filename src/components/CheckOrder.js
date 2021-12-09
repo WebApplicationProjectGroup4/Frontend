b9c860c6ba45512610e41e879cdaa56e22ba387c
Joel Hyyppä <joel.hyyppa@gmail.com>
2021-12-09 16:36:28 +0200

    Order history ui updated

--- src/components/CheckOrder.js
+++ src/components/CheckOrder.js
@@ -1,29 +1,78 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from '../styles/Menu.module.css'
+
 const axios = require('axios').default;
 
-function UpdateOrder(props) {
+var orderArray = [];
 
-    axios.get('http://localhost:3001/orderhistory')
+function UpdateOrder(response) {
+var res = response;
+var dbArray = [];
 
-        .then(function (response) {
-            console.log(response.data);
-          })
-      
-          .catch(function (error) {
-            console.log(error);
-          })
+for(var i = 0; i < res.data.length; i++) {
+  var orderObject = {price: 0, date: 0, idRestaurant: 0, orderedItems: ""};
+
+    orderObject.price = res.data[i].Price;
+    orderObject.date = res.data[i].Date;
+    orderObject.idRestaurant = res.data[i].Restaurants_idRestaurant;
+    orderObject.orderedItems = res.data[i].OrderedItems;
+
+    dbArray.push(orderObject);
+
+}
+if (res.data === "No orders by this account!"){
+   console.log("error");
+}
+
+  orderArray = dbArray;
+  console.log(orderArray);
+} 
+
+class CheckOrder extends React.Component {
+
+  constructor(props) {
+  super();
+  this.state = { data: []
+  };
+  }
+async componentDidMount(){
+
+  await axios.get('http://localhost:3001/orderhistory')
+
+      .then(function (response) {
+          UpdateOrder(response);
+        })
     
-return (
+        .catch(function (error) {
+          console.log(error);
+        })
+
+        this.setState({ data: orderArray });
+      }
+
+      render() {
+        const content = orderArray.map((orderObject) =>
+    
+    <div className={styles.container}>
+      <div className={styles.prices}>Price:   </div>
+      <div className={styles.prices}>{orderObject.price}€</div>
+      <div className={styles.product}>Date:   </div>
+      <div className={styles.product}>{orderObject.date}</div>
+      <div className={styles.product}>Food:    </div>
+      <div className={styles.product}>{orderObject.orderedItems}</div>
+    </div>
+  );
+  return (
+    <div>
+      <div className={styles.title}>Orders</div>
+      {content}
   <div className="Login">
-    <div className="Details">
-        See your previous orders<br />
-        
-      </div>
-    <input className="Button" type="button" value={'Check'} onClick={UpdateOrder}/><br />
     <Link to="/" ><button class="Button" > Close </button> </Link> 
+  </div>
   </div>
 );
 }
+}
 
-export default UpdateOrder;
+export default CheckOrder;
